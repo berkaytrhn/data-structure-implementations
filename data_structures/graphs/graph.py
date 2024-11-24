723fc117b2faaa9cb0463e8296b6df3c048c9366
Berkay <berkaytrhn@gmail.com>
2024-11-24 15:54:41 +0300

    graph basic impl.

--- data_structures/graphs/graph.py
+++ data_structures/graphs/graph.py
@@ -1,3 +1,5 @@
+from typing import Dict, Any
+
 class Node:
     """
     Simple Node for Graph
@@ -5,11 +7,12 @@ class Node:
     def __init__(self, value) -> None:
         self.value = value
         self.neighbours=list()
-        self.distance = float("inf")
+        self.distance=float("inf")
         self.parent = None
         
     def add_neighbour(self, neighbour: "Node"):
-        self.neighbours.append(neighbour)
+        if neighbour not in self.neighbours:
+            self.neighbours.append(neighbour)
 
     def __repr__(self):
         """Representation method"""
@@ -17,18 +20,28 @@ class Node:
 
 class Graph:
     
-    root: Node=None
-    nodes: list = None
-    
-    def __init__(self, root: Node) -> None:
-        self.root=root
-        self.nodes=list()
-    
+    def __init__(self, directed: bool) -> None:
+        self.nodes: Dict[Any, Node]=dict()
+        self.directed=directed
 
-    def add_edge(self):
+    def add_node(self, value: Any):
+        # instead of traversing, we use dict with values as key
+        if value not in self.nodes:
+            self.nodes[value] = Node(value)
+        return self.nodes[value]
+
+    def add_edge(self, value1: Any, value2: Any):
         # traverse and add node 
-        pass
+        node1 = self.add_node(value1)
+        node2 = self.add_node(value2)
+        node1.add_neighbour(node2)
+        if not self.directed:
+            node2.add_neighbour(node1)
     
-    def add_node(self, value):
-        # traverse
-        pass
+    def get_node(self, value: Any) -> Node:
+        return self.nodes.get(value, None) # if not available, None
+    
+    
+    def display(self):
+        for node in self.nodes.values():
+            print(f"Node: {node.value}; Neighbours: {', '.join(node.neighbours)}")
